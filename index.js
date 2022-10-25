c08830185f0bc43ab26c10622b318dc2b5d79b78
Fahim-05 <fahim45147@gmail.com>
2022-10-25 12:33:25 +0600

    course data loaded

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const cors = require('cors');
 
 app.use(cors());
 
+const courseOptions = require('./data/courseOptions.json');
+const course = require('./data/course.json');
+
 
 
 app.get('/', (req, res) =>{
     res.send('Knowledge API running')
+})
+
+app.get('/course-options', (req, res) =>{
+    res.send(courseOptions);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(course => course.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
